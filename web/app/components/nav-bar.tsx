0bc2265c2df3770bc3723fc0d3d00d60265e7674
Benjamin Etornam <navidben45@gmail.com>
2025-01-01 10:40:41 +0000

    use section and add z-index

--- web/app/components/nav-bar.tsx
+++ web/app/components/nav-bar.tsx
@@ -1,22 +1,29 @@
 import { Float } from "~/ui/float";
 import { Logo } from "~/ui/logo";
 import { FloatThemeSwitcher, ThemeSwitcher } from "~/ui/theme-switcher";
+import { Section } from "./section";
+import { Button } from "~/ui/button";
 
 export const NavBar = () => {
     return (
-        <nav className="z-[66666666] border-1.5 mx-auto flex items-center justify-between max-w-7xl bg-[#ddd]/50 dark:(bg-[#333]/50 border-[#333]) backdrop-blur-lg p-3 sticky top-2 rounded-full">
-            <Logo />
-            <ul className="flex gap-3 text-lg text-black dark:text-white">
-                {Links.map((link) => (
-                    <li key={link.path} className={"flex items-center gap-2 " + link?.style}>
-                        {link.icon && <div className={`${link.icon} `}></div>}
-                        <a href={link.path}>{link.label}</a>
-                    </li>
-                ))}
-            </ul>
-            <FloatThemeSwitcher />
+        <nav className="mx-auto sticky top-0 z-[66666666]">
+            <Section>
+                <div className=" border-1.5 shadow-md flex items-center justify-between bg-[#eee]/50 dark:(bg-[#333]/50 border-[#333]) backdrop-blur-md p-3  rounded-full">
+                    <Logo />
+                    <ul className="flex gap-3 text-lg text-black dark:text-white">
+                        {Links.map((link) => (
+                            <li key={link.path} className={"flex items-center gap-2 " + link?.style}>
+                                {link.icon && <div className={`${link.icon} `}></div>}
+                                <a href={link.path}>{link.label}</a>
+                            </li>
+                        ))}
+                    </ul>
+                    <Button>
+                        Register
+                    </Button>
+                </div>
+            </Section>
         </nav>
-
     )
 }
 
