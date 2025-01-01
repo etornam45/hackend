d786af796053b8266544e3d0abba9f5b5a29da7c
Benjamin Etornam <navidben45@gmail.com>
2025-01-01 10:47:51 +0000

    add Competition section compoenent for home page

---
+++ web/app/components/home/competition-section.tsx
@@ -0,0 +1,25 @@
+import { Button } from "~/ui/button"
+import { Section } from "../section"
+import { CompetitionCard } from "./competition-card"
+
+export const CompetitionsSection = () => {
+    return (<Section>
+        <div className="my-[100px]">
+
+            <h2 className="mb-6 text-2xl font-bold">Hackathons for you</h2>
+            <div className="grid gap-5 grid-cols-2">
+                <CompetitionCard />
+                <CompetitionCard />
+                <CompetitionCard />
+                <CompetitionCard />
+                <CompetitionCard />
+                <CompetitionCard />
+            </div>
+            <a href="/competition" className="mt-6">
+                <Button>
+                    View all hackathons
+                </Button>
+            </a>
+        </div>
+    </Section>)
+}
